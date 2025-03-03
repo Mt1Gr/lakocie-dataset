d3c15e9600cc699dc921126c918109d34552f786
Mateusz Gancarek <mateusz.gancarek@outlook.com>
2025-03-03 19:30:13 +0100

    chore: move files according to purpose

--- test/test_scrap/test_permissions.py
+++ test/test_scrap/test_permissions.py
@@ -1,4 +1,4 @@
-from lakocie_dataset import permissions
+from lakocie_dataset.scrap import permissions
 
 ALLOWED_URL = "https://finance.yahoo.com/?guccounter=1&guce_referrer=aHR0cHM6Ly9kdWNrZHVja2dvLmNvbS8&guce_referrer_sig=AQAAAK3CHYa7WftAqrm00xvsX1tj3n_FHm5STsfby0s_6n6xhVQO25apiAGUF7CfnrRnzGIECJWYXf_c8HuBGNajgUqeV_ZgWgvi5dWde_17Vzy-rZWoD3Ksv5LSUSlteTGR4_y3HtF-DMBaLh0nMhtDbPR8Y76geLtU7DFXC_PgXXYW"
 DISALLOWED_URL = "https://www.google.com/travel/flights"
