a18dc16c9bcdf35c493e26c62cace998707dcb59
Mateusz Gancarek <mateusz.gancarek@outlook.com>
2025-03-03 19:54:24 +0100

    test: improve unit test `add_base_url_in_return`

---
+++ test/test_scrap/test_string_utils.py
@@ -0,0 +1,16 @@
+from lakocie_dataset.scrap import string_utils
+
+
+def test_add_base_url_in_return():
+    @string_utils.add_base_url_in_return("https://www.shop.com/")
+    def ret_url(prod_name: str | None) -> str | None:
+        if prod_name is None:
+            return None
+        return f"/{prod_name}"
+
+    assert ret_url("product") == "https://www.shop.com/product"
+    assert ret_url("/product2") == "https://www.shop.com/product2"
+    assert ret_url("product3/") == "https://www.shop.com/product3/"
+    assert ret_url("/product4/") == "https://www.shop.com/product4/"
+    assert ret_url("") == "https://www.shop.com/"
+    assert ret_url(None) is None
