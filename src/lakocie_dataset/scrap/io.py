ef6223d99a8c240243631237a712ba54cf681afc
Mateusz Gancarek <mateusz.gancarek@outlook.com>
2025-03-04 14:09:01 +0100

    refactor: change io params into Path type

--- src/lakocie_dataset/scrap/io.py
+++ src/lakocie_dataset/scrap/io.py
@@ -1,16 +1,30 @@
 import os
-
+from . import paths
 from bs4 import BeautifulSoup
 
 
-def save_content(content: str, filename: str, dir_save_path: str) -> str:
-    """Save content to a file. Return path to the file. Extension **must** be specified"""
-    with open(f"{dir_save_path}/{filename}", "w") as file:
+def save_content(content: str, path: paths.Path) -> paths.Path:
+    """Save content to a file. Return path to the saved file."""
+    if not isinstance(path, paths.Path):
+        raise TypeError(f"Expected Path object, got {type(path)}")
+    elif not path.parent.exists():
+        raise FileNotFoundError(f"Directory {path.parent} not found.")
+    elif path.exists():
+        raise FileExistsError(f"File {path} already exists.")
+
+    with open(path, "w") as file:
         file.write(content)
-    return os.path.join(dir_save_path, filename)
+    return path
 
 
-def html_file_to_soup(path: str) -> BeautifulSoup:
+def html_file_to_soup(path: paths.Path) -> BeautifulSoup:
     """Return BeautifulSoup object from html file path."""
+    if not isinstance(path, paths.Path):
+        raise TypeError(f"Expected Path object, got {type(path)}")
+    elif not os.path.exists(path):
+        raise FileNotFoundError(f"File {path} not found.")
+    elif not path.name.endswith(".html"):
+        raise ValueError(f"File {path} is not an html file.")
+
     with open(path) as file:
         return BeautifulSoup(file, "html.parser")
--- test/test_scrap/test_io.py
+++ test/test_scrap/test_io.py
@@ -1,29 +1,53 @@
-from lakocie_dataset.scrap import io
-
-import os
+from lakocie_dataset.scrap import io, paths
+import pytest
 import tempfile
 
 FILENAME = "example.html"
 
 
 def test_save_content():
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        dir_save_path = tmpdirname
-        path = os.path.join(dir_save_path, FILENAME)
-        content = "Example content"
-        save_path = io.save_content(content, FILENAME, dir_save_path)
-        assert os.path.exists(path)
-        assert save_path == path
-        with open(path) as file:
+    content = "Hello, World!"
+    with tempfile.TemporaryDirectory() as tmpdir:
+        path = paths.Path(tmpdir) / FILENAME
+        saved_path = io.save_content(content, path)
+        assert saved_path == path
+        assert saved_path.exists()
+        with open(saved_path) as file:
             assert file.read() == content
 
 
+def test_save_content_raises():
+    not_existing_parent = paths.Path("not_existing")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        already_existing_file = paths.Path(tmpdir) / FILENAME
+        already_existing_file.touch()
+
+        with pytest.raises(FileNotFoundError):
+            io.save_content("content", not_existing_parent / FILENAME)
+
+        with pytest.raises(FileExistsError):
+            io.save_content("content", already_existing_file)
+
+
 def test_html_file_to_soup():
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        dir_save_path = tmpdirname
-        path = os.path.join(dir_save_path, FILENAME)
-        content = "<html><body>Example content</body></html>"
+    content = "<html><body><h1>Hello, World!</h1></body></html>"
+    with tempfile.TemporaryDirectory() as tmpdir:
+        path = paths.Path(tmpdir) / FILENAME
         with open(path, "w") as file:
             file.write(content)
-        soup: io.BeautifulSoup = io.html_file_to_soup(path)
-        assert soup.body.text == "Example content" if soup.body else False
+        soup = io.html_file_to_soup(path)
+        h1_tag = soup.find("h1") if soup else None
+        assert h1_tag
+        assert h1_tag.text == "Hello, World!"
+
+
+def test_html_file_to_soup_raises():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        not_existing_file = paths.Path(tmpdir) / "not_existing.html"
+        with pytest.raises(FileNotFoundError):
+            io.html_file_to_soup(not_existing_file)
+
+        not_html_file = paths.Path(tmpdir) / "not_html.txt"
+        not_html_file.touch()
+        with pytest.raises(ValueError):
+            io.html_file_to_soup(not_html_file)
